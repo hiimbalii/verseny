62595cbf258e8f0cbeb29efddf2e5d77775d3a4d
hiimbalii <tcrbt1@gmail.com>
2020-04-18 10:50:38 +0000

    bulma added

---
+++ sites/default/files/php/twig/5e9ad9a33aa38_input--bulma.html.twig_47LaA2N-puCbmgbRsD8b1tDbh/-OByVs6Whkx0bZCCG5QAOAGKeXwHcI9-H94CVhQUBAI.php
@@ -0,0 +1,113 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/bulma/templates/elements/input--bulma.html.twig */
+class __TwigTemplate_30b7a0a269d6f0cea5a8d56a68926e583942ac494989b0cadec581cefb017126 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 16, "if" => 23];
+        $filters = ["escape" => 22];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 15
+        echo "  ";
+        // line 16
+        $context["classes"] = [0 => "input"];
+        // line 20
+        echo "
+<div class=\"control";
+        // line 21
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(((($context["icon"] ?? null)) ? (" has-icons-left") : ("")));
+        echo "\">
+    <input";
+        // line 22
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+        echo " />";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null)), "html", null, true);
+        echo "
+    ";
+        // line 23
+        if (($context["icon"] ?? null)) {
+            // line 24
+            echo "        <span class=\"icon is-left\">
+            <i class=\"fa fa-";
+            // line 25
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["icon"] ?? null)), "html", null, true);
+            echo "\"></i>
+        </span>
+    ";
+        }
+        // line 28
+        echo "</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/bulma/templates/elements/input--bulma.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  83 => 28,  77 => 25,  74 => 24,  72 => 23,  66 => 22,  62 => 21,  59 => 20,  57 => 16,  55 => 15,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/bulma/templates/elements/input--bulma.html.twig", "/var/www/html/core/themes/bulma/templates/elements/input--bulma.html.twig");
+    }
+}
